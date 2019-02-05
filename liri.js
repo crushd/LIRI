2dd703bd75933ef49fcb1c77b8a97ab1a6bfaef1
Cody Rush <codyrush@gmail.com>
2019-02-05 10:06:30 -0800

    axios code framework

--- liri.js
+++ liri.js
@@ -1,1 +1,14 @@
 var axios = require("axios");
+
+var queryUrl = "https://www.google.com";
+
+
+axios.get(queryUrl).then(function(err,data) {
+
+    if (err) {
+        console.log(err);
+    }
+
+});
+
+console.log("Hello World.");
